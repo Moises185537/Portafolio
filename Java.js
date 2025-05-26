a681accc1d135a20ee41751a09593f701068f310
Moises185537 <185537@upslp.edu.mx>
2025-05-25 23:15:09 -0600

    Add files via upload

--- Java.js
+++ Java.js
@@ -1,8 +1,16 @@
  function generarPDF() {
         // Obtener los valores del formulario
-        const nombre = document.getElementById('nombre').value;
-        const email = document.getElementById('email').value;
+        const nombre = document.getElementById('Nombre').value;
+        const ApPaterno = document.getElementById('ApPaterno').value;
+        const ApMaterno = document.getElementById('ApMaterno').value;
+        const pass = document.getElementById('pass').value;
+        const fechareg = document.getElementById('fechareg').value;
+        const zona = document.getElementById('zona').value
+        const pago = document.getElementById('pago').value;
+        const Calle = document.getElementById('Calle').value;
         const Nomsalon = document.getElementById('Nomsalon').value;
+        const Colonia = document.getElementById('Colonia').value;
+        const email = document.getElementById('email').value;
 
         // Crear una instancia de jsPDF
         const { jsPDF } = window.jspdf;
@@ -10,8 +18,16 @@
 
         // Agregar contenido al PDF
         doc.text(`Nombre: ${nombre}`, 10, 10);
-        doc.text(`Email: ${email}`, 10, 20);
-        doc.text(`Nombre del salon: ${Nomsalon}`, 10, 30);
+        doc.text(`Apellido Paterno: ${ApPaterno}`, 10, 20);
+        doc.text(`Apellido Materno: ${ApMaterno}`, 10, 30);
+        doc.text(`Codigo de Seguridad: ${pass}`, 10, 40);
+        doc.text(`Fecha de evento: ${fechareg}`, 10, 50);
+        doc.text(`Zona del evento: ${zona}`, 10, 60);
+        doc.text(`Forma del pago: ${pago}`, 10, 70);
+        doc.text(`Calle: ${Calle}`, 10, 80);
+        doc.text(`Nombre del salon: ${Nomsalon}`, 10, 90);
+        doc.text(`Colonia: ${Colonia}`, 10, 100);
+        doc.text(`E-mail: ${email}`, 10, 110);
 
         // Guardar el PDF
         doc.save('Ticket.pdf');
